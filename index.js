da93b2a3e38ca0b33ebd05e55a4e20fe322c7c32
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-09-07 04:53:56 +0530

    added changes

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const urouter = require('./router/universtiesRouter.js');
 require('./utils/config.js');
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '1000mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 app.use('/api/v1/query', queryroute);
 app.use('/resources', express.static(path.join(__dirname, 'uploads')));
